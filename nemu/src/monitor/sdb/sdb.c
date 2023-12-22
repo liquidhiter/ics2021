f5d7e49418e9027bf8fa6096544a25564bfe47aa
tracer-ics2021 <tracer@njuics.org>
2023-12-22 20:36:13 +0100

    >  compile 201220000    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: dad5412b60074e05ab49885d1eeffe5b            Boot ID: 6efc6f2e48864a429eadb0b2e51edaa3   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-26-amd64       Architecture: x86-64  20:36:13 up  6:27,  2 users,  load average: 2.12, 1.63, 1.36

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -5,6 +5,9 @@
 #include "sdb.h"
 
 #include <stdlib.h>
+#include <errno.h>
+
+#define DEV_LOG 1
 
 static int is_batch_mode = false;
 
@@ -89,6 +92,102 @@ static int cmd_help(char *args) {
   return 0;
 }
 
+enum {SI_STEP_SINGLE = 0x001, SI_STEP_MULTIPLE = 0x010, SI_STEP_INVALID = 0x100, };
+static int parse_si_arg(char* args, uint64_t* step) {
+
+  /* step by default points to NULL */
+  step = NULL;
+
+  /* CASE 1: no argument is found after parsing the command line argument by strtok */
+  if (NULL == args) {
+#ifdef DEV_LOG
+    Log("DEV LOG: parse_si_arg: no argument is found after parsing the command line argument by strtok\n");
+#endif /*DEV_LOG*/
+    return SI_STEP_SINGLE;
+  }
+
+  uint64_t next_pos;
+  uint8_t next_char;
+
+  /* Read out the leading whitespaces */
+  for (next_pos = 0; (next_char = *(args + next_pos)) == ' '; next_pos++);
+
+  /* Check the first non-whitespace character */
+  if (next_char == '0') {
+#ifdef DEV_LOG
+    Log("DEV LOG: parse_si_arg: argument only contains whitespaces\n");
+#endif /*DEV_LOG*/
+    return SI_STEP_SINGLE;
+  }
+
+  /* CASE 3: non-whitespace argument(s) is found, which can be invalid though */
+  if (next_char != '[') {
+#ifdef DEV_LOG
+    Log("DEV LOG: parse_si_arg: invalid argument is found: %c", next_char);
+#endif /*DEV_LOG*/
+    return SI_STEP_INVALID;
+  }
+  /* Points to the next character */
+  next_pos++;
+
+  /* Read out the leading spaces before the digit if exists */
+  for (; (next_char = *(args + next_pos)) == ' '; next_pos++);
+
+  /* Check the first non-whitespace character */
+  if (next_char < '0' || next_char > '9') {
+#ifdef DEV_LOG
+    Log("DEV LOG: parse_si_arg: invalid argument is found: %c", next_char);
+#endif /*DEV_LOG*/
+    return SI_STEP_INVALID;
+  }
+  /* Points to the next digit */
+  next_pos++;
+
+  /* Get the number of steps from the argument starting from next_pos
+   * 1. Only digits are allowed to appear
+   * 2. Maximum 20 characters considering maximum value of uint64_t, should be handled by strtoull()
+   */
+  char step_digits = ' ';
+  for (; (next_char = *(args + next_pos)) != ']' && next_char != '\0'; next_pos++, *step_digits += next_char);
+  
+  if (next_char == '\0') {
+#ifdef DEV_LOG
+    Log("DEV LOG: parse_si_arg: invalid argument is found: %s", step_digits);
+#endif /*DEV_LOG*/
+    return SI_STEP_INVALID;
+  }
+
+  /* Convert the string to the number */
+  char *end;
+  uint64_t step_val;
+  /* Ignore all errors happening before */
+  errno = 0;
+  step_val = strtoull(step_digits, &end, 10);
+  if (end == step_digits || *end != '\0' || ERANGE == error) {
+#ifdef DEV_LOG
+    Log("DEV LOG: parse_si_arg: invalid argument is found: %s", step_digits);
+#endif /*DEV_LOG*/
+    return SI_STEP_INVALID;
+  }
+
+  /* Given number of steps must be valid */
+  *step = step_val;
+  /* Points to the next character */
+  next_pos++;
+
+  /* Read out all tailing whitespaces appending after ']' if exists */
+  for (; (next_char = *(args + next_pos)) == ' '; next_pos++);
+
+  /* Argument is considered as invalid even the given digits are perfectly correct if there is trailing characters other than end char */
+  if (next_char != '\0') {
+#ifdef DEV_LOG
+    Log("DEV LOG: parse_si_arg: invalid arguments are appending after ]: %c", next_char);
+    return SI_STEP_INVALID;
+  }
+
+  return SI_STEP_MULTIPLE;
+}
+
 static int cmd_si(char *args) {
   Log("Given argument is: %s\n", args);
   
@@ -98,19 +197,15 @@ static int cmd_si(char *args) {
    * 3. not allow the argument to contain more than one whitespace when there is no step number, e.x. si(\s)*
    *    this is to simplify argument parsing
    */
-  
-  /* Step is defined to be uint64_t type: see function signature of cpu_exec */
-  uint64_t step;
-  if (NULL == args) {
-    Log("Step Execution Tracer: single step execution with no step number given\n");
-    step = 1;
-  } else {
-    char *arg_str = strtok(args, "[]");
-    Log("argument str = %s\n", arg_str);
+  uint64_t step_val;
+  int parse_result = parse_si_arg(args, &step_val);
+  if (SI_STEP_INVALID == parse_result) {
+    return 1;
   }
 
-  Log("Step Execution Tracer: given step = %lu\n", step);
-
+  /* Call cpu_exec to execute given number of steps */
+  Log("Execute %lu number of steps\n", step_val);
+  cpu_exec(step_val);
 
   return 0;
 }
