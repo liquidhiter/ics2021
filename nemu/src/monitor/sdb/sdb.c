33e40159fffd0175ffa0671181356bc9cc346b7d
tracer-ics2021 <tracer@njuics.org>
2023-12-22 20:53:23 +0100

    >  compile 201220000    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: dad5412b60074e05ab49885d1eeffe5b            Boot ID: 6efc6f2e48864a429eadb0b2e51edaa3   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-26-amd64       Architecture: x86-64  20:53:23 up  6:44,  2 users,  load average: 1.17, 1.74, 1.79

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -103,7 +103,7 @@ static int parse_si_arg(char* args, uint64_t* step) {
 #ifdef DEV_LOG
     Log("DEV LOG: parse_si_arg: no argument is found after parsing the command line argument by strtok\n");
 #endif /*DEV_LOG*/
-    *step = 1LU;
+    *step = 1U;
     return SI_STEP_SINGLE;
   }
 
