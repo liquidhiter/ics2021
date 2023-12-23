c5fcab289bc7f0a35b46a2c593ee69cad90fbdfc
tracer-ics2021 <tracer@njuics.org>
2023-12-23 21:59:56 +0100

    >  compile 201220000    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: dad5412b60074e05ab49885d1eeffe5b            Boot ID: 073e38581bfa49919d4722f82f0bb66a   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-26-amd64       Architecture: x86-64  21:59:56 up  6:22,  1 user,  load average: 1.53, 1.21, 1.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,4 +1,11 @@
+#include "debug.h"
 #include <isa.h>
+
+#include <stdlib.h>
+#include <errno.h>
+
+/* Temporarily enable the unit test in file */
+// #define UNIT_TEST_EXPR
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
@@ -11,7 +18,7 @@ enum {
   TK_DECIMAL,
   TK_PARENTHESIS_LEFT,
   TK_PARENTHESIS_RIGHT,
-  
+  TK_NONE, /*Representing the empty (NONE) operator */
   /* TODO: Add more token types */
 
 };
@@ -109,12 +116,15 @@ static bool make_token(char *e) {
             break;
           case TK_DECIMAL:
             newToken.type = TK_DECIMAL;
+#if 0
             /* Limit the number of digits to 32 at most to avoid buffer overflow */
             /* Simple solution: substr_len %= 33; */
             if (substr_len > 32) {
               Log("WARNING: token buffer overflow, actual number of decimal digits is %d", substr_len);
               substr_len = 32;
             }
+#endif
+            Assert(substr_len <= 32, "token buffer overflow, actual number of decimal digits is %d", substr_len);
             strncpy(newToken.str, substr_start, substr_len);
             break;
           default:
@@ -132,6 +142,184 @@ static bool make_token(char *e) {
   return true;
 }
 
+static bool check_parentheses(uint32_t p, uint32_t q, bool *valid) {
+  /* Assume parentheses in the given expression are matched by default */
+  *valid = true;
+
+  /* Check that parentheses are matched with each other at first */
+  int parentheses[q - p + 1];
+  /* Initialize the array with all 0 by default */
+  memset(parentheses, 0, sizeof parentheses);
+  int next_pos = 0;
+  
+  for (int i = p; i <= q; ++i) {
+    Token curr_token = tokens[i];
+    if (curr_token.type == TK_PARENTHESIS_LEFT) {
+      parentheses[next_pos++] = TK_PARENTHESIS_LEFT;
+    } else if (curr_token.type == TK_PARENTHESIS_RIGHT) {
+      if (next_pos == 0) {
+        return false;
+      }
+
+      if (parentheses[next_pos - 1] == TK_PARENTHESIS_LEFT) {
+        --next_pos;
+      } else {
+        return false;
+      }
+    } else {
+      ;
+    }
+  }
+
+  if (next_pos > 0) {
+    *valid = false;
+  }
+
+  return next_pos > 0 && (tokens[p].type!= TK_PARENTHESIS_LEFT || tokens[q].type!= TK_PARENTHESIS_RIGHT);
+}
+
+static uint32_t convert_to_uint32(const char *str) {
+  if (NULL == str) {
+    panic("Unexpected NULL pointer");
+  }
+
+  /* Convert the string to the number */
+  char *end;
+  uint32_t val;
+  /* Ignore all errors happening before */
+  errno = 0;
+  val = strtoul(str, &end, 10);
+  
+  /*Sanity check: ensure the numeric operand is usable */
+  Assert(end == str || *end != '\0' || ERANGE == errno, "strtoul failed: given numeric operand is invalid %s", str);
+
+  return val;
+}
+
+/**
+ * @brief Compare two operators by following the rules:
+ *        1. supported operators: +, -, *, /
+ *        2. + and - owns lower precedence than * and /
+ *        3. + and - or * and / owns the same precedence, but considering as lower precedence because
+ *           op2 is assumed to be the operand at a higher index
+ * 
+ * @param op1 
+ * @param op2 
+ * @return true 
+ * @return false 
+ */
+static bool comp_operator_precedence(uint32_t op1, uint32_t op2) {
+  bool is_lower_precedence = false;
+  switch (op1) {
+    case '+':
+    case '-':
+      is_lower_precedence = (op2 != '(' && op2 != ')') && !(op2 == '*' || op2 == '/');
+      break;
+    case '*':
+    case '/':
+      is_lower_precedence = (op2 != '(' && op2 != ')') && (op2 == '*' || op2 == '/');
+      break;
+    default: Assert(0, "Unsupported operator: %c", op1);
+  }
+
+  return is_lower_precedence;
+}
+
+/**
+ * @brief Find the position of the main operator in the given expression
+ * @note  main operator is the last operator to be processed in an expression
+ * @param p 
+ * @param q 
+ * @return uint32_t 
+ */
+static uint32_t find_main_op(uint32_t p, uint32_t q) {
+  int prev_pos = 0;
+  int curr_pos = 0;
+  int main_op_pos = 0;
+  int main_op = TK_NONE;
+
+  for (curr_pos = p; curr_pos <= q; ++curr_pos) {
+    Token curr_token = tokens[curr_pos];
+    if (curr_token.type == TK_NOTYPE || curr_token.type == TK_DECIMAL) {
+      continue;
+    }
+
+    /* Operator adjacent to the left parentheses can never be the main operator */
+    if (tokens[prev_pos].type == TK_PARENTHESIS_LEFT) {
+      /* Update the previous position */
+      prev_pos = curr_pos;
+      continue;
+    }
+
+    /* Compare the current operator with the previously found one to determine the precedence */
+    if (TK_NONE == main_op) {
+      main_op = curr_token.type;
+      main_op_pos = curr_pos;
+    } else if (comp_operator_precedence(main_op, curr_pos)) {
+        main_op = curr_token.type;
+        main_op_pos = curr_pos;
+    }
+
+    prev_pos = curr_pos;
+  }
+
+  return main_op_pos;
+}
+
+/** To avoid unnecessary calculation when the expression if found to be invalid,
+  * check the flag variable, i.e. success after each time of calling the function
+  * the code looks a bit ugly though ...
+  **/
+static uint32_t eval(uint32_t p, uint32_t q, bool *success) {
+  /* Indication of the validity of parentheses matching in the expression [p, q] */
+  bool is_paren_valid = true;
+
+  /* Sanity check before processing the expression */
+  if (!(*success)) {
+    return 0;
+  }
+
+  if (p > q) {
+    /* Bad expression */
+    *success = false;
+    return 0;
+  } else if (p == q) {
+    /* Single token.
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+    return convert_to_uint32(tokens[p].str);
+  } else if (check_parentheses(p, q, &is_paren_valid) == true) {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    if (!is_paren_valid) {
+      *success = false;
+      return 0;
+    }
+
+    /* Only return the value when the expression is invalid */
+    uint32_t ret_val = eval(p + 1, q - 1, success);
+    if (!(*success)) {
+      return 0;
+    }
+
+    return ret_val;
+  } else {
+    uint32_t main_op = find_main_op(p, q);
+    uint32_t val_left = eval(p, main_op - 1, success);
+    uint32_t val_right = eval(main_op + 1, q, success);
+
+    switch(tokens[main_op].type) {
+      case '+': *success = true; return val_left + val_right;
+      case '-': *success = true; return val_left - val_right;
+      case '*': *success = true; return val_left * val_right;
+      case '/': *success = true; return val_left / val_right;
+      default: Assert(0, "Unsupported operator: %c", tokens[main_op].type);
+    }
+  }
+}
+
 
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
@@ -139,8 +327,19 @@ word_t expr(char *e, bool *success) {
     return 0;
   }
 
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  /* Evaluate the expression, note success indicates the validity of the result */
+  return eval(0, nr_token - 1, success);
+}
 
+/*============================= Unit Tests =============================*/
+#ifdef UNIT_TEST_EXPR
+static void test_make_token(void) {
+
+}
+
+
+int main(void) {
+  test_make_token();
   return 0;
 }
+#endif
